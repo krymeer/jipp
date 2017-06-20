04042815a2a0b55a1e975863fc301ca93e3545e4
krymeer <krymeer@gmail.com>
2017-06-20 20:35:47 +0200

    Better version of split function in Haskell finished

--- lista3/zadanie2.hs
+++ lista3/zadanie2.hs
@@ -1,9 +1,5 @@
--- xh - pierszy element listy
--- yh - drugi element listy
--- xyt - ogon listy (może być pusty)
-
-split :: [a] -> ([a], [a])
-split [] = ([], [])
-split [x] = ([x], [])
-split (xh:yh:xyt) = (xh:xt, yh:yt)
-  where (xt, yt) = split xyt
+split :: [a] => ([a], [a])
+split xs = (ys, zs)
+  where {
+    ys = [y | (y, k) <- zip xs [0..], k `mod` 2 == 0]; 
+    zs = [z | (z, k) <- zip xs [0..], k `mod` 2 == 1]}
